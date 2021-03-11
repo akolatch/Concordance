64396bac2ffbfd4818047fb1ff45a99bf98baf5a
akolatch <aaron.kolatch@gmail.com>
2021-03-11 16:06:33 -0500

    finished coding controllers for pages (still need to integrate)

--- page_controller.py
+++ page_controller.py
@@ -1,18 +1,17 @@
-from flask import redirect, render_template, request, session, Markup
-
-from models import Page_Model, Journal_Model, User_Model, Invite_Model, Members_Model
-from helpers import concordance_list
-
+# check if req and red are needed
+from flask import render_template, session, Markup, request, redirect
+from models import Page_Model, Journal_Model
+from controllers import render_index
+from helpers import concordance_list, concordance_search
 
 Page = Page_Model()
 Journal = Journal_Model()
-user = session["user_id"]
 
-# /load_page POST --> /pages GET
+# /load_page POST --> /pages/load POST
 #  replace page_loader(page_id, concordance_id, warning="")
 
 
-def open_page(page_id, concordance_id=False, warning=""):
+def open_page(user, page_id, concordance_id=False, warning=""):
     """open page"""
 
     concordance_page = None
@@ -65,7 +64,7 @@ def open_page(page_id, concordance_id=False, warning=""):
 
     # for name in name_check:
     #     if page_name.lower() == name["title"].lower():
-    #         return index("The page you are trying to create alread exsists")
+    #         return render_index("The page you are trying to create alread exsists")
 
     # db.execute("INSERT INTO pages (title, journal_id) VALUES (:title, :journal_id)",
     #           title=page_name, journal_id=journal_id)
@@ -82,14 +81,14 @@ def create_page(page_name, journal_id):
         {'title': page_name, 'journal_id': journal_id}, ['id'])
 
     if name_check:
-        return index("The page you are trying to create alread exsists")
+        return render_index("The page you are trying to create alread exsists")
 
     page_id = Page.create({{'title': page_name, 'journal_id': journal_id}})
 
     return open_page(page_id)
 
 
-# /save_page POST --> /pages/save POST (/page PUT)
+# /save_page POST --> /pages/save POST
 # Replace:
 
     #     checker = db.execute("SELECT title, journal_id FROM pages WHERE id = :page_id", page_id=page_id)
@@ -107,7 +106,7 @@ def create_page(page_name, journal_id):
     #     db.execute("UPDATE pages SET (title, content) = (:title, :text)  WHERE id = :page_id", title=title, text=text, page_id=page_id)
     #     return page_loader(page_id, -1, "saved")
 
-# /save_page POST --> /pages/save POST (/page PUT)
+# /save_page POST --> /pages/save POST
 def update_page(title, page_id, text):
 
     journal_id = Page.find({'id': page_id}, ['journal_id'])[0]["journal_id"]
@@ -115,14 +114,15 @@ def update_page(title, page_id, text):
     checker = Page.find(
         {'journal_id': journal_id, 'title': title}, ['id'])[0]['id']
 
-    if checker AND chacker != page_id:
+    if checker and chacker != page_id:
         Page.update({'content': text}, {'id': page_id})
         return open_page(page_id, False, "The new title for this page already exsits. The page was save under its old title.")
 
-    if title == checker[0]["journal_id"]:
-        title_taken = True
+    # if title == checker[0]["journal_id"]:
+    #      title_taken = True
 
     Page.update({'title': title, 'content': text}, {'id': page_id})
+
     return page_loader(page_id, False, "saved")
 
 
@@ -132,14 +132,14 @@ def update_page(title, page_id, text):
     #                          page_id=page_id, user_id=session["user_id"])
 
     #     if not checker:
-    #         return index("You cannot delete a page in journal if you are not an admin")
+    #         return render_index("You cannot delete a page in journal if you are not an admin")
 
     #     db.execute("DELETE FROM pages WHERE id = ?", page_id)
-    #     return index("Page Deleted")
+    #     return render_index("Page Deleted")
 
 
-# /delete_page POST --> /pages/delete POST (/page DELETE)
-def delete_page(page_id):
+# /delete_page POST --> /pages/delete POST
+def delete_page(user, page_id):
     """Delete a page"""
 
     checker = Journal.find_by_page_creater(user)
@@ -152,9 +152,90 @@ def delete_page(page_id):
 
 # Note: the rount should now return
 #     message = delete_page(page_id)
-#     return index(message)
+#     return render_index(message)
 
 
-# /concordance POST --> /pages/concordance GET
+# /concordance POST --> /pages/concordance POST
 # ONLY CHANGE:
     # return open_page(page_id, concordance_id)
+
+
+# /search Post --> /pages/search SET
+# Replace:
+
+    # temp_results = db.execute( "SELECT title, id, pages.journal_id FROM pages JOIN journal_members ON pages.journal_id = journal_members.journal_id WHERE title = :search_term ORDER BY title ASC",
+    #                           search_term=search_term)
+
+    # search_results = concordance_search(search_term, session["user_id"])
+
+    # # Combine results
+    # temp_results += search_results
+
+    # if temp_results == []:
+    #     return render_index("No Results Found!")
+    # if len(temp_results) == 1:
+    #     return page_loader(temp_results[0]["id"], -1)
+
+    # results = []
+    # for page in temp_results:
+    #     if page not in results:
+    #         results.append(page)
+
+    # # Sett warning
+    # warning = ""
+    # display_warning = "none"
+
+    # # Select all pages the user has access to
+    # pages = db.execute("SELECT id, pages.journal_id, title FROM pages JOIN journal_members ON pages.journal_id = journal_members.journal_id WHERE user_id = :user_id ORDER BY title ASC",
+    #                   user_id=session["user_id"])
+
+    # # Select all journals a user has access to
+    # journals = db.execute("SELECT id, name FROM journals JOIN journal_members ON journals.id = journal_members.journal_id  WHERE user_id = :user_id ORDER BY name ASC",
+    #                       user_id=session["user_id"])
+
+    # username = db.execute("SELECT username FROM users WHERE id = :user_id",
+    #                       user_id=session["user_id"])
+    # user_journal = db.execute("SELECT id FROM journals WHERE name = :username",
+    #                           username=username[0]["username"])
+
+    # num_journals = len(journals)
+    # # new user check
+    # if not pages and num_journals == 1:
+    #     new_user = True
+
+    #     # load new_user index
+    #     return render_template("index.html", display_warning=display_warning, warning=warning, username=username, user_journal=user_journal, new_user=new_user, journals=journals)
+    # else:
+    #     new_user = False
+
+    # # check for invites
+    # invites = db.execute("SELECT journal_id, sender FROM  journal_invites WHERE user_id = :user_id", user_id=session["user_id"])
+
+    # search = True
+
+    # # load index
+    # return render_template("index.html", search=search, results=results, invites=invites, display_warning=display_warning, warning=warning, username=username, user_journal=user_journal, new_user=new_user, journals=journals, pages=pages)
+
+
+# /search Post --> /pages/search SET
+def page_search(user, search_term):
+    """execute a search"""
+
+    search_results = Page.find_by_search_term(search_term)
+
+    concordance_search_results = concordance_search(search_term, user)
+
+    # Combine results
+    search_results += concordance_search_results
+
+    if search_results == []:
+        return render_index("No Results Found!")
+    if len(search_results) == 1:
+        return page_loader(temp_results[0]["id"], -1)
+
+    results = []
+    for page in temp_results:
+        if page not in results:
+            results.append(page)
+
+    return render_index('', True, results)
